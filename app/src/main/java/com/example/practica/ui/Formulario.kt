f9604e4a3050f673bc2ae29c0a4049d86d6b5c32
johan <joh.sotob@duocuc.cl>
2025-10-01 12:45:29 -0300

    se sube la act

--- app/src/main/java/com/example/practica/ui/Formulario.kt
+++ app/src/main/java/com/example/practica/ui/Formulario.kt
@@ -3,9 +3,12 @@ package com.example.practica.ui.theme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,9 +17,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+
 @Composable
 fun Formulario(){
-
+    var showDialog by remember { mutableStateOf(false) }
     var username by remember { mutableStateOf("") }
     var pass by remember { mutableStateOf("") }
 
@@ -42,8 +46,22 @@ fun Formulario(){
             },
             label = {Text("Introduce tu contraseña")}
         )
-        Button(onClick = { }) {
+        Button(onClick = {
+            showDialog = true
+        }) {
             Text(text = "Enviar")
         }
+    }
+    if (showDialog){
+        AlertDialog(
+            onDismissRequest = { },
+            title = { Text("sadasdas")},
+            text = { Text("sadsa")},
+            confirmButton = {
+                TextButton(onClick = { showDialog = false}) {
+                    Text("ok")
+                }
+            }
+        )
     }
 }
