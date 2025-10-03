7b8c37d271d9ad31001f27593947dc163161dcec
johan <joh.sotob@duocuc.cl>
2025-10-03 15:44:59 -0300

    act

--- app/src/main/java/com/example/practica/MainActivity.kt
+++ app/src/main/java/com/example/practica/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.practica.ui.screens.Imagen
+import com.example.practica.ui.screens.Navegacion
 import com.example.practica.ui.theme.Formulario
 import com.example.practica.ui.theme.PracticaTheme
 
@@ -18,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticaTheme {
-                Formulario()
+                Navegacion()
             }
         }
     }
